f90ab101ac3fb857d9fd7e400b76616dcf0cc225
Lucas Paiva <lucaspaiva@live.com>
2024-09-21 19:43:56 -0300

    Add function to read CSV files from disk and load data into memory

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -2,7 +2,11 @@
 
 #include <filesystem>
 #include <sstream>
+#include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
 
 using namespace std;
 
@@ -42,3 +46,53 @@ string Util::getLineContent(int line, string content) {
 string Util::getExecutableDirectory() {
     return filesystem::canonical("/proc/self/exe").parent_path().string();
 }
+
+vector<string> Util::rowTokenizer(const string& row) {
+    char separator = ',';
+    stringstream ss(row);
+    string token;
+    vector<string> tokens;
+
+    while (getline(ss, token, separator)) {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
+void Util::printColsData(const unordered_map<string, vector<string>>& cols_data, const vector<string>& cols_names) {
+    for (const string& col_name : cols_names) {
+        cout << "Dados da coluna: " << col_name << ":" << endl;
+        for (const string& dado : cols_data.at(col_name))
+            cout << dado << " ";
+        cout << "\n\n";
+    }
+}
+
+unordered_map<string, vector<string>> Util::loadCSVData(ifstream& target_file, vector<string>& cols_names) {
+        unordered_map<string, vector<string>> cols_data;
+        string row;
+        bool header_row = true;
+
+        while (getline(target_file, row)) {
+            vector<string> row_cells = rowTokenizer(row);
+
+            if (header_row) {
+                cols_names = row_cells;
+                for (const string& name : cols_names)
+                    cols_data[name] = vector<string>();
+                header_row = false;
+                continue;
+            }
+
+            if (row_cells.size() != cols_names.size()) {
+                cerr << "A linha: " << row << " tem um campo não preenchido" << endl;
+                exit(1);
+            }
+
+            for (size_t i = 0; i < row_cells.size(); i++)
+                cols_data[cols_names[i]].push_back(row_cells[i]);
+        }
+
+        return cols_data;
+    }
--- src/util/util.hpp
+++ src/util/util.hpp
@@ -3,6 +3,8 @@
 
 #include <optional>
 #include <string>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 namespace Util {
@@ -13,6 +15,12 @@ namespace Util {
     string getLineContent(int line, string content);
 
     string getExecutableDirectory();
+
+    vector<string> rowTokenizer(const string& row);
+
+    void printColsData(const unordered_map<string, vector<string>>& cols_data, const vector<string>& cols_names);
+    
+    unordered_map<string, vector<string>> loadCSVData(ifstream& target_file, vector<string>& cols_names);
 }  // namespace Util
 
 #endif  // __UTIL_HPP__
